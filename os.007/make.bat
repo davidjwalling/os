f7d0d372096bb2a32fd64213701b1bb8192bad19
dwalling <david.walling@harman.com>
2019-08-20 16:28:50 -0500

    add exit, quit, shutdown

--- os.007/make.bat
+++ os.007/make.bat
@@ -1,8 +1,16 @@
-del /Q os.dat
+@echo off
+if exist os.dat del /Q os.dat
+@echo on
 nasm os.asm -f bin -o os.dat -l os.dat.lst -DBUILDBOOT
-del /Q os.dsk
+@echo off
+if exist os.dsk del /Q os.dsk
+@echo on
 nasm os.asm -f bin -o os.dsk -l os.dsk.lst -DBUILDDISK
-del /Q os.com
+@echo off
+if exist os.com del /Q os.com
+@echo on
 nasm os.asm -f bin -o os.com -l os.com.lst -DBUILDCOM
-del /Q osprep.com
+@echo off
+if exist osprep.com del /Q osprep.com
+@echo on
 nasm os.asm -f bin -o osprep.com -l osprep.com.lst -DBUILDPREP
